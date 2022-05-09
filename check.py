7579491b8949c2b181b48f620cb52b89a2ac89f2
Camerooooon <cjpuffnstuff@gmail.com>
2022-05-08 20:10:53 -0700

    Add while loop so it runs forever

--- check.py
+++ check.py
@@ -2,31 +2,35 @@ import requests
 import hashlib
 import datetime
 import os
+import time
 
+DELAY=5
 WEBSITE_URL=os.environ["CH_WEBSITE_URL"]
 OUT_DIR=os.environ["CH_OUT_DIR"]
 
-print("Time is: " + str(datetime.datetime.now()) + " checking website for changes")
+print("Script startup :)")
 
-r = requests.get(WEBSITE_URL)
-if r.status_code != 200:
-    print("Failed! " + r.text)
-else:
-    print("Success, saving..")
-
-latest_md5 = hashlib.md5(r.content).digest()
-with open(OUT_DIR + "latest_md5", "r") as f:
-    md5 = f.readline()
-    print(md5 + " compared to " + str(latest_md5))
-    if (md5 == str(latest_md5)):
-        print("Unchanged")
-        exit()
-
-with open(OUT_DIR + "latest_md5", "w+") as f:
-    f.write(str(latest_md5))
-
-with open(OUT_DIR + str(datetime.datetime.now()) + ".pdf", "w+b") as f:
-    print("Writing out file")
-    f.write(r.content)
-    f.close()
-
+while True:
+    time.sleep(DELAY)
+    print("Time is: " + str(datetime.datetime.now()) + " checking website for changes")
+    r = requests.get(WEBSITE_URL)
+    if r.status_code != 200:
+        print("Failed! " + r.text)
+    else:
+        print("Success, saving..")
+    
+    latest_md5 = hashlib.md5(r.content).digest()
+    with open(OUT_DIR + "latest_md5", "r") as f:
+        md5 = f.readline()
+        print(md5 + " compared to " + str(latest_md5))
+        if (md5 == str(latest_md5)):
+            print("Unchanged")
+            continue
+    
+    with open(OUT_DIR + "latest_md5", "w+") as f:
+        f.write(str(latest_md5))
+    
+    with open(OUT_DIR + str(datetime.datetime.now()) + ".pdf", "w+b") as f:
+        print("Writing out file")
+        f.write(r.content)
+        f.close()
